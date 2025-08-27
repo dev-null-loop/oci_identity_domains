d21dd48143aa5263158277c4d4222add70d227a3
dev-null-loop <dev.null.loop@gmail.com>
2025-08-27 11:44:37 +0300

    user: fix schema variable

--- user/variables.tf
+++ user/variables.tf
@@ -25,3 +25,8 @@ variable "display_name" {
   type        = string
   default     = null
 }
+
+variable "schemas" {
+  description = "(Required) (Updatable) REQUIRED. The schemas attribute is an array of Strings which allows introspection of the supported schema version for a SCIM representation as well any schema extensions supported by that representation. Each String value must be a unique URI. This specification defines URIs for User, Group, and a standard \"enterprise\" extension. All representations of SCIM schema MUST include a non-zero value array with value(s) of the URIs supported by that representation. Duplicate values MUST NOT be included. Value order is not specified and MUST not impact behavior."
+  type        = list(string)
+}
