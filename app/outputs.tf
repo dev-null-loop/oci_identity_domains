a31a71747da14dda70df23e83c1685ffd2a13fba
dev-null-loop <dev.null.loop@gmail.com>
2025-10-13 12:19:35 +0300

    app: +client_id output

--- app/outputs.tf
+++ app/outputs.tf
@@ -2,6 +2,11 @@ output "id" {
   value = oci_identity_domains_app.this.id
 }
 
+output "application_id" {
+  value = oci_identity_domains_app.this.id
+}
+
+
 output "idcs_endpoint" {
   value = oci_identity_domains_app.this.idcs_endpoint
 }
@@ -10,6 +15,13 @@ output "name" {
   value = oci_identity_domains_app.this.name
 }
 
+output "client_id" {
+  value = oci_identity_domains_app.this.name
+}
+
 output "ocid" {
   value = oci_identity_domains_app.this.ocid
+}
+output "client_secret" {
+  value = oci_identity_domains_app.this.client_secret
 }
