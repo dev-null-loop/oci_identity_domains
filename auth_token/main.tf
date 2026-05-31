42bfc5074bb3c00f56b9b263a4768db66e58692a
dev-null-loop <57629276+dev-null-loop@users.noreply.github.com>
2026-05-31 11:24:40 +0300

    Add auth_token module

---
+++ auth_token/main.tf
@@ -0,0 +1,35 @@
+resource "oci_identity_domains_auth_token" "this" {
+  attribute_sets               = var.attribute_sets
+  attributes                   = var.attributes
+  authorization                = var.authorization
+  description                  = var.description
+  expires_on                   = var.expires_on
+  idcs_endpoint                = var.idcs_endpoint
+  ocid                         = var.ocid
+  resource_type_schema_version = var.resource_type_schema_version
+  schemas                      = var.schemas
+  status                       = var.status
+  dynamic "tags" {
+    for_each = var.tags != null ? var.tags : []
+    iterator = ta
+    content {
+      key   = ta.value.key
+      value = ta.value.value
+    }
+  }
+  dynamic "urnietfparamsscimschemasoracleidcsextensionself_change_user" {
+    for_each = var.urnietfparamsscimschemasoracleidcsextensionself_change_user[*]
+    iterator = ucu
+    content {
+      allow_self_change = ucu.value.allow_self_change
+    }
+  }
+  dynamic "user" {
+    for_each = var.user[*]
+    iterator = us
+    content {
+      ocid  = us.value.ocid
+      value = us.value.value
+    }
+  }
+}
---
+++ auth_token/outputs.tf
@@ -0,0 +1,14 @@
+output "id" {
+  description = "The identifier of the resource."
+  value       = oci_identity_domains_auth_token.this.id
+}
+
+output "ocid" {
+  description = "The ocid of the resource."
+  value       = oci_identity_domains_auth_token.this.ocid
+}
+
+output "status" {
+  description = "The current status of the resource."
+  value       = oci_identity_domains_auth_token.this.status
+}
