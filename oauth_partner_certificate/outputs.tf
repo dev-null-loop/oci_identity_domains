052c2d68ef49a44d36077ffe3051864509a13c2a
dev-null-loop <57629276+dev-null-loop@users.noreply.github.com>
2026-05-31 11:25:31 +0300

    Add oauth_partner_certificate module

---
+++ oauth_partner_certificate/outputs.tf
@@ -0,0 +1,9 @@
+output "id" {
+  description = "The identifier of the resource."
+  value       = oci_identity_domains_oauth_partner_certificate.this.id
+}
+
+output "ocid" {
+  description = "The ocid of the resource."
+  value       = oci_identity_domains_oauth_partner_certificate.this.ocid
+}
