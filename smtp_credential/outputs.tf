bafdfa0afd9d64e22708428823ded9c0d939f369
dev-null-loop <57629276+dev-null-loop@users.noreply.github.com>
2026-05-31 11:25:42 +0300

    Add smtp_credential module

---
+++ smtp_credential/outputs.tf
@@ -0,0 +1,14 @@
+output "id" {
+  description = "The identifier of the resource."
+  value       = oci_identity_domains_smtp_credential.this.id
+}
+
+output "ocid" {
+  description = "The ocid of the resource."
+  value       = oci_identity_domains_smtp_credential.this.ocid
+}
+
+output "status" {
+  description = "The current status of the resource."
+  value       = oci_identity_domains_smtp_credential.this.status
+}
