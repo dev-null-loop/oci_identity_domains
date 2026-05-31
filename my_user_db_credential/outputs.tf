610f16738069ce790f1a756e8b93f61c6eee34e9
dev-null-loop <57629276+dev-null-loop@users.noreply.github.com>
2026-05-31 11:25:25 +0300

    Add my_user_db_credential module

---
+++ my_user_db_credential/outputs.tf
@@ -0,0 +1,19 @@
+output "id" {
+  description = "The identifier of the resource."
+  value       = oci_identity_domains_my_user_db_credential.this.id
+}
+
+output "name" {
+  description = "The name of the resource."
+  value       = oci_identity_domains_my_user_db_credential.this.name
+}
+
+output "ocid" {
+  description = "The ocid of the resource."
+  value       = oci_identity_domains_my_user_db_credential.this.ocid
+}
+
+output "status" {
+  description = "The current status of the resource."
+  value       = oci_identity_domains_my_user_db_credential.this.status
+}
