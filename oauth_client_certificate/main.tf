13197932feaf3ce770ded4141ea6f39c2dae27c4
dev-null-loop <57629276+dev-null-loop@users.noreply.github.com>
2026-05-31 11:25:30 +0300

    Add oauth_client_certificate module

---
+++ oauth_client_certificate/main.tf
@@ -0,0 +1,22 @@
+resource "oci_identity_domains_oauth_client_certificate" "this" {
+  authorization                = var.authorization
+  certificate_alias            = var.certificate_alias
+  external_id                  = var.external_id
+  idcs_endpoint                = var.idcs_endpoint
+  key_store_id                 = var.key_store_id
+  key_store_name               = var.key_store_name
+  key_store_password           = var.key_store_password
+  map                          = var.map
+  ocid                         = var.ocid
+  resource_type_schema_version = var.resource_type_schema_version
+  schemas                      = var.schemas
+  dynamic "tags" {
+    for_each = var.tags != null ? var.tags : []
+    iterator = ta
+    content {
+      key   = ta.value.key
+      value = ta.value.value
+    }
+  }
+  x509base64certificate = var.x509base64certificate
+}
---
+++ oauth_client_certificate/outputs.tf
@@ -0,0 +1,9 @@
+output "id" {
+  description = "The identifier of the resource."
+  value       = oci_identity_domains_oauth_client_certificate.this.id
+}
+
+output "ocid" {
+  description = "The ocid of the resource."
+  value       = oci_identity_domains_oauth_client_certificate.this.ocid
+}
