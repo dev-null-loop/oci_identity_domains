47e98cbb08da46f6366b39588e593d01b1478d99
dev-null-loop <57629276+dev-null-loop@users.noreply.github.com>
2026-05-31 11:25:18 +0300

    Add my_customer_secret_key module

---
+++ my_customer_secret_key/outputs.tf
@@ -0,0 +1,19 @@
+output "id" {
+  description = "The identifier of the resource."
+  value       = oci_identity_domains_my_customer_secret_key.this.id
+}
+
+output "display_name" {
+  description = "The display_name of the resource."
+  value       = oci_identity_domains_my_customer_secret_key.this.display_name
+}
+
+output "ocid" {
+  description = "The ocid of the resource."
+  value       = oci_identity_domains_my_customer_secret_key.this.ocid
+}
+
+output "status" {
+  description = "The current status of the resource."
+  value       = oci_identity_domains_my_customer_secret_key.this.status
+}
